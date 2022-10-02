31e401770850243fad315dfde3086aac7057e872
FloatingDream528 <82099714+FloatingDream528@users.noreply.github.com>
2022-10-02 21:48:22 +0800

    Update s5.sh

--- s5.sh
+++ s5.sh
@@ -1,2 +1,3 @@
+apt update && apt install wget git curl -y
 wget https://github.com/SagerNet/sing-box/releases/download/v1.1-beta8/sing-box-1.1-beta8-linux-amd64.tar.gz && tar -zxvf sing-box-1.1-beta8-linux-amd64.tar.gz && cd sing-box-1.1-beta8-linux-amd64 && wget https://raw.githubusercontent.com/FloatingDream528/s5/main/config.json
 sing-box run -c config.json &
