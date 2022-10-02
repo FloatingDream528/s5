6e81b9e87374a9f92136e943a418a2819d80b47f
FloatingDream528 <82099714+FloatingDream528@users.noreply.github.com>
2022-10-02 22:24:47 +0800

    Update s5.sh

--- s5.sh
+++ s5.sh
@@ -1,3 +1,5 @@
 apt update && apt install wget git curl -y
-wget https://github.com/SagerNet/sing-box/releases/download/v1.1-beta8/sing-box-1.1-beta8-linux-amd64.tar.gz && tar -zxvf sing-box-1.1-beta8-linux-amd64.tar.gz && cd sing-box-1.1-beta8-linux-amd64 && wget https://raw.githubusercontent.com/FloatingDream528/s5/main/config.json
-sing-box run -c config.json &
+curl -Lo /etc/systemd/system/sing-box.service https://raw.githubusercontent.com/FloatingDream528/s5/main/sing-box.service
+curl -Lo /root/sing-box.tar.gz https://github.com/SagerNet/sing-box/releases/download/v1.1-beta8/sing-box-1.1-beta8-linux-amd64.tar.gz && tar -zxvf sing-box.tar.gz && cd sing-box-1.1-beta8-linux-amd64
+wget https://raw.githubusercontent.com/FloatingDream528/s5/main/config.json
+systemctl start sing-box.service && systemctl enable sing-box.service
